099f466870ca8c4d16968299e30dda307c1c1eb9
SHarrigfeld <hrrgfld@gmail.com>
2020-09-16 14:38:19 -0400

    java code

---
+++ Homework.java
@@ -0,0 +1,12 @@
+public class Homework
+{
+   public static void main (String[] args)
+   {
+      String name = "Billy Willopecker";
+      int age = 14;
+      float height = 6.5f;
+      System.out.println(name);
+      System.out.println(age);
+      System.out.println(height);
+   }
+}
