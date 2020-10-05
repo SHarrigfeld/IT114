f1f68e943d248ac57cce9a01399715de55300a44
SHarrigfeld <hrrgfld@gmail.com>
2020-10-05 15:46:25 -0400

    Java Folder

---
+++ Basics/src/Test.java
@@ -0,0 +1,26 @@
+
+public class Test {
+  
+	public static int sum(int num) {
+		if (num > 0) {
+			return num + sum(num - 1);
+		}
+		return 0;
+	}
+   public static int loop(int num)
+   {
+      int sum=0;
+      for(int i=num;i>0; i--)
+      {
+         sum +=i; 
+      }
+      return sum; 
+   }
+
+	public static void main(String[] args) {
+		System.out.println(sum(10));
+      System.out.println("______________________________");
+      System.out.println(loop(10));
+	}
+   
+}
